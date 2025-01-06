0a95083341cfe72c19a72c4537a049b258f6ab02
Aiden Sheehan <as227mj@student.lnu.se>
2025-01-06 16:52:52 +0100

    initial stuff

---
+++ JavaToCSharp.Tests/BinaryExpressionVisitorTests.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using com.github.javaparser;
+using com.github.javaparser.ast.expr;
+using JavaToCSharp.Expressions;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace JavaToCSharp.Tests
+{
+    public class BinaryExpressionVisitorTests
+    {
+        [Fact]
+        public void ShouldConvertBooleanAnd()
+        {
+            BinaryExpressionVisitor visitor = new BinaryExpressionVisitor();
+            var options = new JavaConversionOptions
+            {
+                IncludeUsings = false,
+                IncludeNamespace = false,
+            };
+            var c = new ConversionContext(options);
+            Expression left = new BooleanLiteralExpr(false);
+            Expression right = new BooleanLiteralExpr(true);
+            BinaryExpr expr = new BinaryExpr(left, right, BinaryExpr.Operator.AND);
+            var actual = BinaryExpressionVisitor.VisitExpression(c, expr);
+
+            var expected = SyntaxFactory.BinaryExpression(SyntaxKind.LogicalAndExpression, SyntaxFactory.LiteralExpression(SyntaxKind.FalseLiteralExpression), SyntaxFactory.LiteralExpression(SyntaxKind.TrueLiteralExpression));
+            Assert.Equal(expected.ToFullString(), actual!.ToFullString());
+        }
+    }
+}
